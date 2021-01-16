06173d9fd6f96befe84f2a4674e5c5ab779c8779
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2021-01-16 17:35:49 +0000

    refactor: make the exercice-04 code cleaner

--- chapter-02/exercises/exercise-04/lib/matchstick_factory.ex
+++ chapter-02/exercises/exercise-04/lib/matchstick_factory.ex
@@ -1,32 +1,23 @@
 defmodule MatchstickFactory do
-  def boxes(matchstick_qty) do
-    big_boxes_qty = div(matchstick_qty, 50)
-    matchsticks_inside_big_boxes = big_boxes_qty * 50
-    reminder_from_big_boxes = get_reminder(matchstick_qty, matchsticks_inside_big_boxes)
+  @size_big 50
+  @size_medium 20
+  @size_small 5
 
-    medium_boxes_qty = div(reminder_from_big_boxes, 20)
-    matchsticks_inside_medium_boxes = medium_boxes_qty * 20
+  def boxes(matchsticks) do
+    big_boxes = div(matchsticks, @size_big)
+    remaining_from_big_boxes = rem(matchsticks, @size_big)
 
-    reminder_from_medium_boxes =
-      get_reminder(reminder_from_big_boxes, matchsticks_inside_medium_boxes)
+    medium_boxes = div(remaining_from_big_boxes, @size_medium)
+    remaining_from_medium_boxes = rem(remaining_from_big_boxes, @size_medium)
 
-    small_boxes_qty = div(reminder_from_medium_boxes, 5)
-    matchsticks_inside_small_boxes = small_boxes_qty * 5
-
-    reminder_from_small_boxes =
-      get_reminder(reminder_from_medium_boxes, matchsticks_inside_small_boxes)
+    small_boxes = div(remaining_from_medium_boxes, @size_small)
+    remaining_from_small_boxes = rem(remaining_from_medium_boxes, @size_small)
 
     %{
-      big: big_boxes_qty,
-      medium: medium_boxes_qty,
-      small: small_boxes_qty,
-      remaining_matchsticks: reminder_from_small_boxes
+      big: big_boxes,
+      medium: medium_boxes,
+      small: small_boxes,
+      remaining_matchsticks: remaining_from_small_boxes
     }
-  end
-
-  defp get_reminder(current_matchstick_qty, 0), do: current_matchstick_qty
-
-  defp get_reminder(current_matchstick_qty, matchsticks_qty_inside_the_box) do
-    rem(current_matchstick_qty, matchsticks_qty_inside_the_box)
   end
 end
